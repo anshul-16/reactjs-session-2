bcdf3a2a3190c28b5ee47210745919219956c91b
Anshul Sachdev <anshul16.sachdev@gmail.com>
2023-07-04 09:37:44 +0530

    useRef() hook : counting the number of renders

--- public/index.js
+++ public/index.js
@@ -6,27 +6,22 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i["return"] && (_r = _i["return"](), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-var Counter = function Counter() {
-  var _React$useState = React.useState(0),
+var App = function App() {
+  var _React$useState = React.useState(""),
     _React$useState2 = _slicedToArray(_React$useState, 2),
-    count = _React$useState2[0],
-    setCount = _React$useState2[1];
-  var _React$useState3 = React.useState(true),
-    _React$useState4 = _slicedToArray(_React$useState3, 2),
-    tempState = _React$useState4[0],
-    setTempState = _React$useState4[1];
+    value = _React$useState2[0],
+    setValue = _React$useState2[1];
+  var count = React.useRef(0);
   React.useEffect(function () {
-    setCount(function (count) {
-      return count + 1;
-    });
-  }, [tempState]);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", null, "Counter : ", count), /*#__PURE__*/React.createElement("button", {
-    onClick: function onClick() {
-      return setTempState(function (prev) {
-        return !prev;
-      });
+    count.current = count.current + 1;
+  });
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    value: value,
+    onChange: function onChange(e) {
+      return setValue(e.target.value);
     }
-  }, "Change Temp State"));
+  }), /*#__PURE__*/React.createElement("div", null, "Render count = ", count.current));
 };
 var root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( /*#__PURE__*/React.createElement(Counter, null));
+root.render( /*#__PURE__*/React.createElement(App, null));
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const Counter = () => {
+const App = () => {
 
-    const [count,setCount] = React.useState(0)
-    const [tempState, setTempState] = React.useState(true);
+    const [value,setValue] = React.useState("")
+    const count = React.useRef(0)
 
     React.useEffect(() => {
-        setCount(count => count+1)
-    },[tempState])
+        count.current = count.current+1   
+    })
 
     return(
         <>
-            <div>Counter : {count}</div>
-            <button onClick={() => setTempState(prev => !prev)}>Change Temp State</button>
+            <input type='text' value={value} onChange={(e) => setValue(e.target.value)} />
+            <div>Render count = {count.current}</div>
         </>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
-root.render(<Counter />);
+root.render(<App />);
