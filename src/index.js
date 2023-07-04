c421e51fdcc1065d5a12fff63dbba329da241421
Anshul Sachdev <anshul16.sachdev@gmail.com>
2023-07-04 08:43:40 +0530

    Updating Objects in a state

--- src/index.js
+++ src/index.js
@@ -1,38 +1,30 @@
-const ParentComp = () => {
-    return(
-        <div>
-            <div>Parent Component</div>
-            <br />
-            <ChildComponent name='Aman' class='6' />
-            <br />
-            <ChildComponent name='Rahul' class='10'/>
-        </div>
-    )
-}
+const Car = () => {
+  const [car, setCar] = React.useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red"
+  });
 
-// const ChildComponent = (props) => {
-    
-//     console.log(props)
-//     return(
-//         <div>
-//             <div>Child info-</div>
-//             <div>Name - {props.name}</div>
-//             <div>Class - {props.class}</div>
-//         </div>
-//     )
-// }
+  const updateColor = () => {
+    setCar(previousState => {
+      return { ...previousState, color: "blue" }
+    });
+  }
 
-class ChildComponent extends React.Component{
-    render() {
-        return(
-            <div>
-                <div>Child info-</div>
-                <div>Name - {this.props.name}</div>
-                <div>Class - {this.props.class}</div>
-            </div>
-        )
-    }
+  return (
+    <>
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
+      <button
+        type="button"
+        onClick={updateColor}
+      >Blue</button>
+    </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
-root.render(<ParentComp />);
+root.render(<Car />);
